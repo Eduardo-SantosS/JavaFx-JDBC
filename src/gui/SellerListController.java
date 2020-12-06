ac26855c662402905dbccb7efee8c0c51f4e900c
Eduardo-SantosS <santos.eduardopro@gmail.com>
2020-12-06 18:29:45 -0300

    SellerList

---
+++ src/gui/SellerListController.java
@@ -0,0 +1,161 @@
+package gui;
+
+import db.DbIntegrityException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Seller;
+import model.services.SellerService;
+import sample.Main;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class SellerListController implements Initializable, DataChangeListener {
+
+    private SellerService service;
+
+    @FXML
+    private TableView<Seller> sellerTableView;
+    @FXML
+    private TableColumn<Seller, Integer> tableColumnId;
+    @FXML
+    private TableColumn<Seller, String> tableColumnName;
+    @FXML
+    private TableColumn<Seller, Seller> tableColumnEdit;
+    @FXML
+    private TableColumn<Seller, Seller> tableColumnRemove;
+    @FXML
+    private Button btnNew;
+    private ObservableList<Seller> obsList;
+
+    @FXML
+    public void onBtnNewAction(ActionEvent event){
+        Stage parentStage = Utils.currentStage(event);
+        Seller seller = new Seller();
+        createDialogForm(seller,"/gui/SellerForm.fxml", parentStage);
+    }
+
+    public void setSellerService(SellerService service){
+        this.service = service;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeNodes();
+
+    }
+
+    private void initializeNodes() {
+        tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        Stage stage = (Stage) Main.getMainScene().getWindow();
+        sellerTableView.prefHeightProperty().bind(stage.heightProperty());
+    }
+
+    public void updateTableView(){
+        if (service == null){
+            throw new IllegalStateException("Service was null");
+        }
+        List<Seller> list = service.findAll();
+        obsList = FXCollections.observableArrayList(list);
+        sellerTableView.setItems(obsList);
+        initEditButtons();
+        initRemoveButtons();
+    }
+
+    private void createDialogForm(Seller seller ,String absoluteName,Stage parentStage){
+//        try {
+//            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+//            Pane pane = loader.load();
+//
+//            SellerFormController controller = loader.getController();
+//            controller.setSeller(seller);
+//            controller.setSellerService(new SellerService());
+//            controller.subscribeDataChangeListener(this);
+//            controller.updateFormData();
+//
+//            Stage dialogStage = new Stage();
+//            dialogStage.setTitle("Enter Seller data");
+//            dialogStage.setScene(new Scene(pane));
+//            dialogStage.setResizable(false);
+//            dialogStage.initOwner(parentStage);
+//            dialogStage.initModality(Modality.WINDOW_MODAL);
+//            dialogStage.showAndWait();
+//
+//        }catch (IOException e){
+//            Alerts.showAlert("IO exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+//        }
+    }
+
+    private void initEditButtons(){
+        tableColumnEdit.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnEdit.setCellFactory(param -> new TableCell<>() {
+            private final Button btn = new Button("edit");
+
+            @Override
+            protected void updateItem(Seller seller, boolean b) {
+                super.updateItem(seller, b);
+
+                if (seller == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(btn);
+                btn.setOnAction(
+                        event -> createDialogForm(seller, "/gui/SellerForm.fxml", Utils.currentStage(event)));
+            }
+        });
+    }
+
+    private void initRemoveButtons(){
+        tableColumnRemove.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnRemove.setCellFactory(param -> new TableCell<>(){
+            private final Button btn = new Button("remove");
+
+            @Override
+            protected void updateItem(Seller seller, boolean b) {
+                super.updateItem(seller, b);
+
+                if (seller == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(btn);
+                btn.setOnAction(event -> removeEntity(seller));
+            }
+        });
+    }
+
+    private void removeEntity(Seller seller) {
+        Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete?");
+
+        if (result.get() == ButtonType.OK){
+            if (service == null){
+                throw new IllegalStateException("Service was null");
+            }
+            try {
+                service.remove(seller);
+                updateTableView();
+            }catch (DbIntegrityException e){
+                Alerts.showAlert("Error removing object", null, e.getMessage(), Alert.AlertType.ERROR);
+            }
+        }
+    }
+
+    @Override
+    public void onDataChanged() {
+        updateTableView();
+    }
+}
---
+++ src/model/services/SellerService.java
@@ -0,0 +1,30 @@
+package model.services;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerService {
+
+    private SellerDao dao = DaoFactory.createSellerDao();
+
+    public List<Seller> findAll(){
+        return dao.findAll();
+    }
+
+    public void saveOrUpdate(Seller seller){
+        if (seller.getId() == null){
+            dao.insert(seller);
+        }else {
+            dao.update(seller);
+        }
+    }
+
+    public void remove(Seller seller){
+        dao.deleteById(seller.getId());
+    }
+}
