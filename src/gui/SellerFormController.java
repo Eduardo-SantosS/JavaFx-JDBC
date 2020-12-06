600192083a928fc63993dcaeb833870c17b1cb01
Eduardo-SantosS <santos.eduardopro@gmail.com>
2020-12-06 20:39:24 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -19,6 +19,7 @@ import model.services.DepartmentService;
 import model.services.SellerService;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.*;
@@ -104,20 +105,39 @@ public class SellerFormController implements Initializable {
     }
 
     private Seller getFormData() {
-        Seller dep = new Seller();
+        Seller seller = new Seller();
 
         ValidationException exception = new ValidationException("Validation error");
-        dep.setId(Utils.tryParseToInt(txtId.getText()));
+        seller.setId(Utils.tryParseToInt(txtId.getText()));
         if (txtName.getText() == null || txtName.getText().trim().equals("")){
-            exception.addError("Name", "Field can't be empty");
+            exception.addError("name", "Field can't be empty");
         }
-        dep.setName(txtName.getText());
+        seller.setName(txtName.getText());
+
+        if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")){
+            exception.addError("email", "Field can't be empty");
+        }
+        seller.setEmail(txtEmail.getText());
+
+        if (dpBirthDate.getValue() == null){
+            exception.addError("birthDate", "Field can't be empty");
+        }else {
+            Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+            seller.setBirthDate(Date.from(instant));
+        }
+
+        if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")){
+            exception.addError("baseSalary", "Field can't be empty");
+        }
+        seller.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+
+        seller.setDepartment(departmentComboBox.getValue());
 
         if (exception.getErrors().size() > 0){
             throw exception;
         }
 
-        return dep;
+        return seller;
     }
 
     @FXML
@@ -170,9 +190,10 @@ public class SellerFormController implements Initializable {
     private void setErrorMessages(Map<String, String > errors){
         Set<String> fields = errors.keySet();
 
-        if (fields.contains("Name")){
-            labelErrorName.setText(errors.get("Name"));
-        }
+        labelErrorName.setText((fields.contains("name") ? errors.get("name"):""));
+        labelErrorEmail.setText((fields.contains("email") ? errors.get("email"):""));
+        labelErrorBirthDate.setText((fields.contains("birthDate") ? errors.get("birthDate"):""));
+        labelErrorBaseSalary.setText((fields.contains("baseSalary") ? errors.get("baseSalary"):""));
     }
 
     private void initializeComboBoxDepartment(){
